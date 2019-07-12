2e1aced65306f95da9636e7259e93f1fd6f54ccf
apolloclark <apolloclark@gmail.com>
2019-07-11 23:06:43 -0400

    added support for 7.2, 7.1, 70, 6.8, 6.7

--- travis_tests.sh
+++ travis_tests.sh
@@ -5,24 +5,29 @@ pipenv install -r test-requirements.txt --python 2.7
 # CentOS 7
 export DOCKER_IMAGE_BASE=centos:7.6.1810
 export DOCKER_INST_NAME_SUFFIX=centos_7.6.1810
+export PACKAGE_VERSION=7.2.0
 pipenv run molecule test --all
 
 # Ubuntu Bionic
 export DOCKER_IMAGE_BASE=ubuntu:bionic
 export DOCKER_INST_NAME_SUFFIX=ubuntu_bionic
+export PACKAGE_VERSION=7.2.0
 pipenv run molecule test --all
 
 # Ubuntu Xenial
 export DOCKER_IMAGE_BASE=ubuntu:xenial
 export DOCKER_INST_NAME_SUFFIX=ubuntu_xenial
+export PACKAGE_VERSION=7.2.0
 pipenv run molecule test --all
 
 # Debian Stretch
 export DOCKER_IMAGE_BASE=debian:stretch
 export DOCKER_INST_NAME_SUFFIX=debian_stretch
+export PACKAGE_VERSION=7.2.0
 pipenv run molecule test --all
 
 # Debian Jessie
 export DOCKER_IMAGE_BASE=debian:jessie
 export DOCKER_INST_NAME_SUFFIX=debian_jessie
+export PACKAGE_VERSION=7.2.0
 pipenv run molecule test --all
